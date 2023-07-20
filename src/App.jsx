61490f6cdccec14e46d705136531e52c396c23db
eugenesay <eugene_say@apecia.com>
2023-07-20 17:47:58 +0800

    test123

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="read-the-docs">
-       Hello World from Amplify HEHEHEHEHEH
+       Hello World from Amplify
       </p>
     </>
   )
