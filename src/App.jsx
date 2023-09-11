3bb37695bc714cea29cd6ecb5efee9fd6aeddd62
eugenesay <eugene_say@apecia.com>
2023-09-11 10:40:45 +0800

    remove amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import { Amplify } from "aws-amplify";
+import "./App.css";
+// import "@aws-amplify/ui-react/styles.css";
+// import awsExports from "./aws-exports";
+// Amplify.configure(awsExports);
+// import { useAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  // const { user, signOut } = useAuthenticator((context) => [
+  //   context.user,
+  //   context.signOut,
+  // ]);
   return (
     <>
       <div>
@@ -21,6 +29,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {/* <button onClick={signOut}>Sign Out</button> */}
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -28,11 +38,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className="read-the-docs">
-       Hello World from Amplify HEHEHEHEHE
-      </p>
+      <p className="read-the-docs">Hello World from Amplify HEHEHEHEHE</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
